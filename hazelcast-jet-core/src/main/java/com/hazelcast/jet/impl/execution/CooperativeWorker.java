215ebb1b15cb9042d91ac4aa00d41f5ffe92e558
Jaromir Hamala <jaromir.hamala@gmail.com>
2019-02-11 19:09:58 +0100

    wip - remove custom COW collection impl

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/execution/CooperativeWorker.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/execution/CooperativeWorker.java
@@ -18,7 +18,6 @@ package com.hazelcast.jet.impl.execution;
 
 import com.hazelcast.internal.metrics.Probe;
 import com.hazelcast.jet.JetException;
-import com.hazelcast.jet.impl.util.CopyOnWriteCollection;
 import com.hazelcast.jet.impl.util.ProgressState;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.LoggingService;
@@ -26,12 +25,13 @@ import com.hazelcast.util.concurrent.BackoffIdleStrategy;
 import com.hazelcast.util.concurrent.IdleStrategy;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 import static com.hazelcast.jet.impl.util.LoggingUtil.logFinest;
 import static com.hazelcast.jet.impl.util.Util.lazyIncrement;
-import static java.lang.Thread.currentThread;
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
@@ -42,62 +42,31 @@ final class CooperativeWorker implements Runnable {
             new BackoffIdleStrategy(0, 0, MICROSECONDS.toNanos(1), MILLISECONDS.toNanos(1));
 
     @Probe(name = "taskletCount")
-    private final CopyOnWriteCollection<TaskletTracker> trackers;
-    private final CopyOnWriteCollection<TaskletTracker>.RefreshableIterator trackerIterator;
+    private final List<TaskletTracker> trackers;
     @Probe
     private final AtomicLong iterationCount = new AtomicLong();
     private final ILogger logger;
     private final AtomicReference<Boolean> gracefulShutdown;
+    private boolean madeProgress;
 
     CooperativeWorker(AtomicReference<Boolean> gracefulShutdown, LoggingService loggingService) {
-        this.trackers = new CopyOnWriteCollection<>();
-        this.trackerIterator = trackers.iterator();
+        this.trackers = new CopyOnWriteArrayList<>();
         this.logger = loggingService.getLogger(CooperativeWorker.class);
         this.gracefulShutdown = gracefulShutdown;
     }
 
     @Override
     public void run() {
-        final Thread thread = currentThread();
         long idleCount = 0;
+        TrackerConsumer trackerConsumer = new TrackerConsumer();
         while (true) {
             Boolean gracefulShutdownLocal = gracefulShutdown.get();
             // exit condition
             if (gracefulShutdownLocal != null && (!gracefulShutdownLocal || trackers.isEmpty())) {
                 break;
             }
-            boolean madeProgress = false;
-            trackerIterator.refresh();
-            while (trackerIterator.hasNext()) {
-                TaskletTracker t = trackerIterator.next();
-                long start = 0;
-                if (logger.isFinestEnabled()) {
-                    start = System.nanoTime();
-                }
-                try {
-                    thread.setContextClassLoader(t.getJobClassLoader());
-                    final ProgressState result = t.getTasklet().call();
-                    if (result.isDone()) {
-                        dismissTasklet(t);
-                    } else {
-                        madeProgress |= result.isMadeProgress();
-                    }
-                } catch (Throwable e) {
-                    logger.warning("Exception in " + t.getTasklet(), e);
-                    t.getExecutionTracker().exception(new JetException("Exception in " + t.getTasklet() + ": " + e, e));
-                }
-                if (t.getExecutionTracker().executionCompletedExceptionally()) {
-                    dismissTasklet(t);
-                }
-
-                if (logger.isFinestEnabled()) {
-                    long elapsedMs = NANOSECONDS.toMillis((System.nanoTime() - start));
-                    if (elapsedMs > COOPERATIVE_LOGGING_THRESHOLD) {
-                        logger.finest("Cooperative tasklet call of '" + t.getTasklet() + "' took more than "
-                                + COOPERATIVE_LOGGING_THRESHOLD + " ms: " + elapsedMs + "ms");
-                    }
-                }
-            }
+            madeProgress = false;
+            trackers.forEach(trackerConsumer);
             lazyIncrement(iterationCount);
             if (madeProgress) {
                 idleCount = 0;
@@ -119,5 +88,44 @@ final class CooperativeWorker implements Runnable {
         logFinest(logger, "Tasklet %s is done", t.getTasklet());
         t.getExecutionTracker().taskletDone();
         trackers.remove(t);
+    }
+
+    private final class TrackerConsumer implements Consumer<TaskletTracker> {
+        private final Thread currentThread;
+
+        private TrackerConsumer() {
+            this.currentThread = Thread.currentThread();
+        }
+
+        @Override
+        public void accept(TaskletTracker t) {
+            long start = 0;
+            if (logger.isFinestEnabled()) {
+                start = System.nanoTime();
+            }
+            try {
+                currentThread.setContextClassLoader(t.getJobClassLoader());
+                final ProgressState result = t.getTasklet().call();
+                if (result.isDone()) {
+                    dismissTasklet(t);
+                } else {
+                    madeProgress |= result.isMadeProgress();
+                }
+            } catch (Throwable e) {
+                logger.warning("Exception in " + t.getTasklet(), e);
+                t.getExecutionTracker().exception(new JetException("Exception in " + t.getTasklet() + ": " + e, e));
+            }
+            if (t.getExecutionTracker().executionCompletedExceptionally()) {
+                dismissTasklet(t);
+            }
+
+            if (logger.isFinestEnabled()) {
+                long elapsedMs = NANOSECONDS.toMillis((System.nanoTime() - start));
+                if (elapsedMs > COOPERATIVE_LOGGING_THRESHOLD) {
+                    logger.finest("Cooperative tasklet call of '" + t.getTasklet() + "' took more than "
+                            + COOPERATIVE_LOGGING_THRESHOLD + " ms: " + elapsedMs + "ms");
+                }
+            }
+        }
     }
 }
